276241f5a2d7a5726453db967e331b9269c59dc1
Danny <gbnguyenw@gmail.com>
2025-06-28 22:29:43 -0400

    updated my db.py to look for key based on dev or prod

--- app/db.py
+++ app/db.py
@@ -1,14 +1,19 @@
 import os
 import firebase_admin
+import json
 from firebase_admin import credentials
 from dotenv import load_dotenv
 from firebase_admin import firestore
 
 load_dotenv()
+# 🔑 Works for local AND Vercel
+if os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON"):
+    cert_dict = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON"))
+    cred = credentials.Certificate(cert_dict)
+else:
+    cred = credentials.Certificate(os.getenv("FIRESTORE_PATH_TO_KEY"))
 
-PATH_TO_SERVICE_ACCOUNT_KEY = os.getenv("FIRESTORE_PATH_TO_KEY")
 
-cred = credentials.Certificate(PATH_TO_SERVICE_ACCOUNT_KEY)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
